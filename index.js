da36dee633ae060fc6f484e7b64f19b056c8949e
Alex Boklin <alex.boklin@gmail.com>
2018-12-30 23:22:38 +0200

    Use ApolloLink.from to pass an arbitrary number of links

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // ES6 version
 // import { ApolloClient } from 'apollo-client';
+// import { ApolloLink } from 'apollo-link';
 // import { HttpLink } from 'apollo-link-http';
 // import { InMemoryCache } from 'apollo-cache-inmemory';
 // import gql from 'graphql-tag';
 // import fetch from 'node-fetch';
 const { ApolloClient } = require('apollo-client');
+const { ApolloLink } = require('apollo-link');
 const { HttpLink } = require('apollo-link-http');
 const { InMemoryCache } = require('apollo-cache-inmemory');
 const gql = require('graphql-tag');
 const fetch = require('node-fetch');
 
-const link = new HttpLink({uri: 'https://fakerql.com/graphql', fetch});
+const link = ApolloLink.from([new HttpLink({uri: 'https://fakerql.com/graphql', fetch})]);
 
 const cache = new InMemoryCache();
 
