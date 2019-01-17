1ff4991c4f8a20a1fd27f3248916ccccd74db0cb
Alex Boklin <alex.boklin@gmail.com>
2019-01-17 11:40:57 +0100

    Rename index.js to server.js; add consoleLink

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
 import fetch from 'node-fetch';
+import { consoleLink } from 'apollo-link-log-query';
 
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: ApolloLink.from([new HttpLink({uri: 'https://fakerql.com/graphql', fetch})]),
+  link: ApolloLink.from([consoleLink, new HttpLink({uri: 'https://fakerql.com/graphql', fetch})]),
   cache
 });
 
