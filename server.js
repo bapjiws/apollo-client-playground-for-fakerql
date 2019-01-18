bfc573f8a3517c733e51f642b2277709e33dcf06
Alex Boklin <alex.boklin@gmail.com>
2019-01-18 11:19:02 +0100

    [WIP] Add preliminary Express setup to server.js

--- server.js
+++ server.js
@@ -24,3 +24,15 @@ client.query({query: gql`
 `})
   .then(data => console.log(data))
   .catch(error => console.error(error));
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+//
+// app.use(express.static(path.join(__dirname, 'build')));
+//
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+//
+// app.listen(9000);
