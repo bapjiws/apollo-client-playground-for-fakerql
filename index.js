86b034281d6bcbbc4067d114d7e6972f293b2574
Alex Boklin <alex.boklin@gmail.com>
2019-01-18 11:18:03 +0100

    Create App React component in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+	<h2>My first Apollo app 🚀</h2>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
